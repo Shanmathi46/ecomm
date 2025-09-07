d7e1f0de44d2644fb5378301ac886a11d5b5f572
Hemanth Doddala <hemanth@Hemanths-MacBook-Pro.local>
2025-09-07 16:05:21 -0400

    connected 2 independent applications and loaded into a container

--- cart/src/bootstrap.js
+++ cart/src/bootstrap.js
@@ -1,3 +1,13 @@
 import faker from "faker";
-const cartText=`<div>You have ${faker.random.number()} in your cart</div>`
-document.querySelector('#dev-cart').innerHTML=cartText;
+const mountCart=(el)=>{
+    const cartText=`<div>You have ${faker.random.number()} in your cart</div>`
+    el.innerHTML=cartText
+}
+
+if(process.env.NODE_ENV==='development'){
+    const el=document.querySelector('#dev-cart')
+    if(el){
+        mountCart(el)
+    }
+}
+export {mountCart}
--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,5 +1,7 @@
 import {mount} from "products/ProductsIndex"
-import "cart/CartShow"
+import {mountCart} from "cart/CartShow"
 console.log("Container");
 const el=document.querySelector('#my-products')
 mount(el)
+const el1=document.querySelector('#my-cart')
+mountCart(el1)
